a2b36d9cf2da30a3e92b309ee3983b7ac2e5f8c3
nm86n <nm86nm@gmail.com>
2025-05-08 20:01:12 +0200

    Update project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,41 +48,41 @@ android {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
             dependencies {
-                implementation("androidx.core:core-ktx:1.12.0")
-                implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-                implementation("androidx.activity:activity-compose:1.8.0")
-                implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+                implementation("androidx.core:core-ktx:1.16.0")
+                implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+                implementation("androidx.activity:activity-compose:1.10.1")
+                implementation(platform("androidx.compose:compose-bom:2025.05.00"))
                 implementation("androidx.compose.ui:ui")
                 implementation("androidx.compose.ui:ui-graphics")
                 implementation("androidx.compose.ui:ui-tooling-preview")
                 implementation("androidx.compose.material3:material3")
                 testImplementation("junit:junit:4.13.2")
-                androidTestImplementation("androidx.test.ext:junit:1.1.5")
-                androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-                androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+                androidTestImplementation("androidx.test.ext:junit:1.2.1")
+                androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+                androidTestImplementation(platform("androidx.compose:compose-bom:2025.05.00"))
                 androidTestImplementation("androidx.compose.ui:ui-test-junit4")
                 debugImplementation("androidx.compose.ui:ui-tooling")
                 debugImplementation("androidx.compose.ui:ui-test-manifest")
 
                 // Compose dependencies
-                implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-                implementation ("androidx.navigation:navigation-compose:2.7.4")
+                implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
+                implementation ("androidx.navigation:navigation-compose:2.9.0")
                 implementation ("com.google.accompanist:accompanist-flowlayout:0.17.0")
 
                 // Coroutines
-                implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-                implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+                implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+                implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
                 // Coroutine Lifecycle Scopes
-                implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-                implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+                implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+                implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
 
                 //Dagger - Hilt
-                implementation ("com.google.dagger:hilt-android:2.48")
+                implementation ("com.google.dagger:hilt-android:2.52")
                 kapt ("com.google.dagger:hilt-compiler:2.48")
                 //implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
-                kapt ("androidx.hilt:hilt-compiler:1.0.0")
-                implementation ("androidx.hilt:hilt-navigation-compose:1.1.0-rc01")
+                kapt ("androidx.hilt:hilt-compiler:1.2.0")
+                implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
 
                 // Retrofit
                 implementation ("com.squareup.retrofit2:retrofit:2.9.0")
