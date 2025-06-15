30f5185e282b894cf3f5dca4059c6a8a74a1bf9b
nm86n <nm86nm@gmail.com>
2025-06-15 20:35:03 +0200

    Update project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,9 +47,9 @@ android {
 
     dependencies {
         implementation("androidx.core:core-ktx:1.16.0")
-        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
         implementation("androidx.activity:activity-compose:1.10.1")
-        implementation(platform("androidx.compose:compose-bom:2025.05.01"))
+        implementation(platform("androidx.compose:compose-bom:2025.06.00"))
         implementation("androidx.compose.ui:ui")
         implementation("androidx.compose.ui:ui-graphics")
         implementation("androidx.compose.ui:ui-tooling-preview")
@@ -57,13 +57,13 @@ android {
         testImplementation("junit:junit:4.13.2")
         androidTestImplementation("androidx.test.ext:junit:1.2.1")
         androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-        androidTestImplementation(platform("androidx.compose:compose-bom:2025.05.01"))
+        androidTestImplementation(platform("androidx.compose:compose-bom:2025.06.00"))
         androidTestImplementation("androidx.compose.ui:ui-test-junit4")
         debugImplementation("androidx.compose.ui:ui-tooling")
         debugImplementation("androidx.compose.ui:ui-test-manifest")
 
         // Compose dependencies
-        implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
+        implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
         implementation ("androidx.navigation:navigation-compose:2.9.0")
         implementation ("com.google.accompanist:accompanist-flowlayout:0.17.0")
 
@@ -72,8 +72,8 @@ android {
         implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
         // Coroutine Lifecycle Scopes
-        implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
-        implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+        implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
+        implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
 
         //Dagger - Hilt
         implementation ("com.google.dagger:hilt-android:2.56.2")
